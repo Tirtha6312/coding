7a6b090a1a93aa7c97d8f6348e1f1217e413be4b
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-07-03 11:57:18 +0530

    Time: 573 ms (100%), Space: 60.2 MB (31.82%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.py
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.py
@@ -1,69 +1,68 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
         n = len(online)
 
         graph = [[] for _ in range(n)]
         indegree = [0] * n
+        costs = set()
 
         for u, v, w in edges:
             graph[u].append((v, w))
             indegree[v] += 1
+            costs.add(w)
 
-        from collections import deque
-
-        q = deque()
-
-        for i in range(n):
-            if indegree[i] == 0:
-                q.append(i)
-
+        # Topological sort (DAG)
         topo = []
+        q = deque([i for i in range(n) if indegree[i] == 0])
 
         while q:
             u = q.popleft()
             topo.append(u)
 
-            for v, _ in graph[u]:
+            for v, w in graph[u]:
                 indegree[v] -= 1
                 if indegree[v] == 0:
                     q.append(v)
 
-        def check(limit):
-            INF = 10 ** 30
+        costs = sorted(costs)
 
-            dp = [INF] * n
-            dp[0] = 0
+        def can(min_edge):
+            INF = float('inf')
+            dist = [INF] * n
+            dist[0] = 0
 
             for u in topo:
-
-                if dp[u] == INF:
+                if dist[u] == INF:
                     continue
 
+                # intermediate offline nodes not allowed
                 if u != 0 and u != n - 1 and not online[u]:
                     continue
 
                 for v, w in graph[u]:
-
-                    if w < limit:
+                    if w < min_edge:
                         continue
 
-                    if v != n - 1 and not online[v]:
+                    if v != n - 1 and v != 0 and not online[v]:
                         continue
 
-                    if dp[u] + w < dp[v]:
-                        dp[v] = dp[u] + w
+                    if dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
 
-            return dp[-1] <= k
+            return dist[n - 1] <= k
 
-        left = 0
-        right = 10 ** 9
+        left, right = 0, len(costs) - 1
         ans = -1
 
         while left <= right:
             mid = (left + right) // 2
+            val = costs[mid]
 
-            if check(mid):
-                ans = mid
+            if can(val):
+                ans = val
                 left = mid + 1
             else:
                 right = mid - 1
