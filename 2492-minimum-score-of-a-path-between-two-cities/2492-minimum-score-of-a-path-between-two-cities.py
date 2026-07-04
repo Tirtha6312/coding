2084ef4e477feba2bf81a09d7d7a9242e72ecf82
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-07-04 14:25:46 +0530

    Time: 80 ms (99.8%), Space: 51.7 MB (95.03%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,17 +1,29 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parents = list(range(n+1))
+        self.min_weights = [float("inf")] * (n+1)
+
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, x, y, weight):
+        x, y = self.find(x), self.find(y)
+        new_weight = min(self.min_weights[x], self.min_weights[y], weight)
+        if x != y:
+            self.parents[x] = y
+            self.min_weights[y] = new_weight
+            return True
+        else:
+            self.min_weights[x] = new_weight
+            return False
+            
+
 class Solution:
     def minScore(self, n: int, roads: list[list[int]]) -> int:
-        root = list(range(n + 1))
-
-        def find(i: int) -> int:
-            root[i] = find(root[i]) if root[i] != i else i
-            return root[i]
-
-        for x, y, _ in roads:
-            root[find(x)] = find(y)
-
-        res, g1 = 10001, find(1)
-        for x, _, d in roads:
-            if find(x) == g1:
-                res = min(res, d)
-
-        return res
+        uf = UnionFind(n)
+        for u, v, w in roads:
+            uf.union(u, v, w)
+        return uf.min_weights[uf.find(1)]
+            
