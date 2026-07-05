673dd71a8d0ab71074b2ea7d123faeab9d9f0141
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-07-05 12:31:55 +0530

    Time: 92 ms (92.66%), Space: 19.3 MB (100%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.py
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.py
@@ -1,54 +1,28 @@
 class Solution:
     def pathsWithMaxScore(self, board: List[str]) -> List[int]:
-        MOD = 10**9 + 7
-        n = len(board)
+        n = len(board[0])
+        f = [[-inf, 0] for _ in range(n + 1)]
+        f[1] = [0, 1]
+        for j in range(1, n):
+            c = board[0][j]
+            if c == 'X': break
+            f[j + 1] = [f[j][0] + int(c), 1]
 
-        next_score = [-1] * (n + 1)
-        next_ways = [0] * (n + 1)
-
-        for i in range(n - 1, -1, -1):
-            # Fresh arrays store the current row.
-            curr_score = [-1] * (n + 1)
-            curr_ways = [0] * (n + 1)
-
-            for j in range(n - 1, -1, -1):
-                cell = board[i][j]
-
-                if cell == 'X':
+        for i, s in enumerate(board[1:]):
+            tmp = f[0][:]
+            for j, c in enumerate(s):
+                if c == 'X':
+                    tmp = f[j + 1][:]
+                    f[j + 1] = [-inf, 0]
                     continue
-
-                if cell == 'S':
-                    curr_score[j] = 0
-                    curr_ways[j] = 1
-                    continue
-
-                best = max(
-                    next_score[j],
-                    curr_score[j + 1],
-                    next_score[j + 1]
-                )
-
-                if best == -1:
-                    continue
-
-                ways = 0
-
-                if next_score[j] == best:
-                    ways += next_ways[j]
-                if curr_score[j + 1] == best:
-                    ways += curr_ways[j + 1]
-                if next_score[j + 1] == best:
-                    ways += next_ways[j + 1]
-
-                value = 0 if cell == 'E' else int(cell)
-
-                curr_score[j] = best + value
-                curr_ways[j] = ways % MOD
-
-            next_score = curr_score
-            next_ways = curr_ways
-
-        if next_score[0] == -1:
-            return [0, 0]
-
-        return [next_score[0], next_ways[0]]
+                if c in 'ES': c = '0'
+                max_v = max(tmp[0], f[j][0], f[j + 1][0])
+                n_path = 0
+                if max_v == tmp[0]: n_path += tmp[1]
+                if max_v == f[j][0]: n_path += f[j][1]
+                if max_v == f[j + 1][0]: n_path += f[j + 1][1]
+                tmp = f[j + 1][:]
+                f[j + 1] = [max_v + int(c), n_path % 1000000007]
+        res = f[-1]
+        if res[0] < 0: res[0] = 0
+        return res
