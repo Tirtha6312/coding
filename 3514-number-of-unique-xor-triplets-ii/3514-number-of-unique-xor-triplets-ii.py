bfd5eee63dac282727b97ef1bfc1053ac07a5ac6
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-07-24 10:53:14 +0530

    Time: 735 ms (92.59%), Space: 19.9 MB (11.11%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
@@ -1,19 +1,15 @@
 class Solution:
-    def uniqueXorTriplets(self, nums: List[int]) -> int:
-        m = max(nums)
-        u = 1
-        while u <= m:
-            u <<= 1
-        one = [False] * u
-        two = [False] * u
-        three = [False] * u
-        for v in nums:
-            one[v] = True
-            for x in range(u):
-                if one[x]:
-                    two[x ^ v] = True
-        for v in nums:
-            for x in range(u):
-                if two[x]:
-                    three[x ^ v] = True
-        return sum(1 for b in three if b)
+    def uniqueXorTriplets(self, nums: list[int]) -> int:
+
+        update = lambda x: num^x
+
+        pairs, trips = set([0]), set(nums),  
+        digits = 2** max(nums).bit_length()
+
+        while nums:
+            num = nums.pop()
+            trips|= set(map(update, pairs))
+            pairs|= set(map(update, nums))
+            if len(trips) == digits: break
+ 
+        return len(trips)
