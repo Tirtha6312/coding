e764b3aea4964dc38c5673fef7910afecc2a15c1
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-07-30 11:30:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (83.33%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -1,23 +1,10 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        freq = Counter(word)
 
-        max_heap = []
+        pushes = 0
 
-        for f in freq.values():
-            heapq.heappush(max_heap, -f) 
+        for i in range(len(word)):
 
-        ans = 0
-        index = 0
+            pushes += (i // 8) + 1
 
-        while max_heap:
-
-            frequency = -heapq.heappop(max_heap)
-
-            presses = index // 8 + 1
-
-            ans += frequency * presses
-
-            index += 1
-
-        return ans
+        return pushes
