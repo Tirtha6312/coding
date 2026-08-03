b95e0976aa5f3972fd6181088d3cfed1dbed4d8d
TIRTHA MUKHERJEE <156400798+Tirtha6312@users.noreply.github.com>
2026-08-03 15:32:51 +0530

    Time: 317 ms (97.24%), Space: 23.2 MB (99.35%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,21 +1,14 @@
 class Solution:
-    s = ["Bob", "Tie", "Alice"]
-    def stoneGameIII(self, A: List[int]) -> str:
-        n = len(A)
-
-        @cache
-        def maxDiff(i: int) -> int:
-            if i == n: return 0
-            a = b = c = -5e7
-
-            if i < n:
-                a = A[i] - maxDiff(i + 1)
-            if i + 1 < n:
-                b = A[i] + A[i + 1] - maxDiff(i + 2)
-            if i + 2 < n:
-                c = A[i] + A[i + 1] + A[i + 2] - maxDiff(i + 3)
-
-            return max(a, b, c)
-
-        d = maxDiff(0)
-        return self.s[(d > 0) - (d < 0) + 1]
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        
+        cache = [0] * (len(stoneValue) + 4)
+        cache[0] = stoneValue[0]
+        length = len(stoneValue)
+        stoneValue.extend([0, 0, 0])
+        for i in range(length - 1, -1, -1):
+            cache[i] = max(stoneValue[i] - cache[i + 1], stoneValue[i] + stoneValue[i+1] - cache[i + 2], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - cache[i+ 3])
+        if cache[0] > 0:
+            return "Alice"
+        elif cache[0] < 0:
+            return "Bob"
+        return "Tie"
